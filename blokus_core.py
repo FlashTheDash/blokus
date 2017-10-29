11a6ebeb6c3ea9f557ee41aec8c635ae11f6f919
Oskar Wolff <owolff@student.csdspartans.org>
2017-10-29 16:45:13 -0400

    completed Piece class?

--- blokus_core.py
+++ blokus_core.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
 '''
 This file holds all the core classes necessary to play a blokus game 
 '''
+
+import json
+
 
 class Piece():
     '''
@@ -25,15 +27,48 @@ class Piece():
 
     '''
     
-    def __init__(self, color='red', squares=[[0,0]], corners=[[0,0]], 
-                 rotation=0, flipped=False):
+    def __init__(self, color='red', squares=[[0,0]], rotation=0, flipped=False):
         self.color = color
         self.value = len(squares)
+
         self.squares = squares
-        self.corners = corners
+        self.edges = self.get_edge_squares()
+        self.corners = self.get_corner_squares()
 
         self.rotation = 0
         self.flipped = False
+    
+    def get_edge_squares(self):
+        '''
+        uses the squares list to create a list of squares where other pieces of
+        the same color cannot go
+        '''
+        edges = []
+        for square in self.squares[:]:
+            # please forgive me
+            # finds the adjacent squares to each square
+            possible_edges = [[square[0]+a,square[1]+b] for a, b, square in [(1,0,square),(-1,0,square),(0,1,square),(0,-1,square)]]
+            for edge in possible_edges:
+                if edge not in self.squares:
+                    edges.append(edge)
+        return edges
+
+
+    def get_corner_squares(self):
+        '''
+        uses the squares list and the edges list to create a list of squares
+        where other pieces of the same color can go
+        '''
+        corners = []
+        for square in self.squares[:]:
+            # please forgive me
+            # finds the diagonal squares to each square
+            possible_corners = [[square[0]+a,square[1]+b] for a, b, square in [(1,1,square),(1,-1,square),(-1,1,square),(-1,-1,square)]]
+            for corner in possible_corners:
+                if corner not in self.squares + self.edges:
+                    corners.append(corner)
+        return corners
+
 
     def collides(self, other):
         other_locations = other.square_locations()
@@ -95,9 +130,24 @@ class Piece():
 class Player():
     '''
     Represents a single player. Used to initialize all the pieces as well.
+    Methods:
+
+    Variables:
+    color       - blue, yellow, red, green
+    score       - a tally of the player's score
+    pieces      - A list of all pieces still in the player's hand
     '''
-    def __init__():
-        pass
+    def __init__(color):
+        self.score = 0
+        self.color = color
+        
+        self.pieces = []
+        # load data from pieces.json
+        with open('pieces.json') as pieces_json:
+            pieces = json.load(pieces_json)
+            for piece in pieces:
+                self.pieces.append(Piece(color=color, squares=piece[0]))
+
 
 
 class Board():
@@ -115,10 +165,17 @@ class Board():
     edges       - 2d array that holds lists of colors that cannot play for
                     each square
     '''
-    def __init__(self, length=20, height=20):
+    def __init__(self, length=20, height=20, num_players=4):
         self.spaces = [[None for _ in range(height)] for _ in range(length)]
-        self.corners = self.spaces[:]
-        self.edges = self.spaces[:]
+        self.edges = [[[] for _ in range(height)] for _ in range(length)]
+        self.corners = self.edges[:]
+        # set the corners to the players
+        self.corners[0][0].append('blue')
+        self.corners[length-1][height-1].append('red')
+        if num_players == 4:
+            self.corners[length-1][0].append('yellow')
+            self.corners[0][height-1].append('green')
+
 
     def is_legal_move(self, piece, origin):
         try:
